82aaad7d6950ae613dff7d6120031d341b4ee533
thienanh1999 <thienanh313@gmail.com>
2020-11-16 17:52:57 +0700

    initialization

---
+++ app/src/main/java/vnd/macro/sot/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package vnd.macro.sot.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vnd.macro.sot.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
